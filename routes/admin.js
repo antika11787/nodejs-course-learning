13388307ef33aae4651ae7669574a03e84c55577
antika <antika.noor@bjitgroup.com>
2024-06-20 09:22:11 +0600

    mvc first commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,11 @@
-const path = require("path");
-const rootDir = require("../utils/path");
-
 const express = require("express");
+
+const productController = require("../controllers/products");
 
 const router = express.Router();
 
-const products = [];
+router.get("/add-product", productController.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product", {
-    pageTitle: "Add product",
-    path: "/admin/add-product", // for pug and ejs
-    formCSS: true, // for handlebars
-    productCSS: true,
-    activeAddProduct: true,
-  });
-});
+router.post("/add-product", productController.postProducts);
 
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
-
-module.exports = {
-  routes: router,
-  products: products,
-};
+module.exports = router;
